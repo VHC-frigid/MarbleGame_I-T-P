65cbcfb77e8ba6f088d06b8bba1d8074fb447f96
VHC-frigid <159389593+VHC-frigid@users.noreply.github.com>
2024-04-22 01:38:19 +1000

    finnished assessment

---
+++ Assets/Scipts/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    private void Start()
+    {
+        UnlockMouse();
+    }
+    void UnlockMouse()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
